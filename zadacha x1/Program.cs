899e519b4057cab213c490445aa0a6203c800f7b
dimka <e.atagazina@pravo.tech>
2023-02-09 20:13:27 +0400

    first commit

---
+++ zadacha x1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+Console.WriteLine("Введите целое число a ");
+int a = int.Parse(Console.ReadLine()!);
+int sum = 0;
+a = Math.Abs(a);
+while (a > 0)
+{
+    sum += a % 10;
+    a = a / 10;
+}
+Console.WriteLine(sum);
