9c1bd98d32f1e0227a037e29129e6cc1be178d49
dimka <e.atagazina@pravo.tech>
2023-02-09 22:25:28 +0400

    first commit

---
+++ zadacha x3/Program3.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+
+int count = 1;
+Console.Write("введте положительное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+while (count <= N)
+{
+    int kub = Convert.ToInt32(Math.Pow(count, 3));
+    if ((count % 2) == 0)
+    {
+    Console.WriteLine (kub);
+    }
+    count++;
+}
