0ec12db8e3b889f54baf09efbee637edaa6c0ff7
dimka <e.atagazina@pravo.tech>
2023-02-09 21:43:16 +0400

    first commit

---
+++ zadacha x2/Program2.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine()!);
+int N1 = 1;
+int count = 1;
+while (N1 <= N)
+{
+    count = (N1 * count);
+    N1++;
+}
+Console.Write($"{count} ");
