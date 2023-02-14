90fa91cf1c9be70e17df96a614cdd8a6cf1de41d
dimka <e.atagazina@pravo.tech>
2023-02-14 21:44:25 +0400

    first commit

---
+++ zadacha34/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. 
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int[] array = GetArray(10, 100, 999);
+int resultEven(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (inArray[i] % 2 == 0) 
+        {
+        count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(String.Join(", ", array));
+int count = resultEven(array);
+Console.WriteLine(count);
