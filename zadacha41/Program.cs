9ca93facba74c1d5ebdc65e4e6d1f9bc7582c9cf
dimka <e.atagazina@pravo.tech>
2023-02-18 20:50:46 +0400

    first commit

---
+++ zadacha41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.Write("Cколько чисел хотите ввести?: ");
+int M = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Чисел больше ноля: {count}");
